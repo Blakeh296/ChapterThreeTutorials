35eb9ae79e9ee1683999dfc92759913d9bc77e46
Blakeh296 <34355831+Blakeh296@users.noreply.github.com>
2018-03-19 15:42:47 -0400

    Add files via upload

---
+++ DistanceTraveled/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace DistanceTraveled
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.outPutDistanceTraveled = new System.Windows.Forms.Label();
+            this.btnFiveHr = new System.Windows.Forms.Button();
+            this.btnEightHrs = new System.Windows.Forms.Button();
+            this.btnTwelveHrs = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.BtnMainMenu = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(78, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(68, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // outPutDistanceTraveled
+            // 
+            this.outPutDistanceTraveled.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.outPutDistanceTraveled.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outPutDistanceTraveled.Location = new System.Drawing.Point(68, 157);
+            this.outPutDistanceTraveled.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.outPutDistanceTraveled.Name = "outPutDistanceTraveled";
+            this.outPutDistanceTraveled.Size = new System.Drawing.Size(112, 28);
+            this.outPutDistanceTraveled.TabIndex = 3;
+            this.outPutDistanceTraveled.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnFiveHr
+            // 
+            this.btnFiveHr.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFiveHr.Location = new System.Drawing.Point(4, 67);
+            this.btnFiveHr.Name = "btnFiveHr";
+            this.btnFiveHr.Size = new System.Drawing.Size(68, 23);
+            this.btnFiveHr.TabIndex = 7;
+            this.btnFiveHr.Text = "5 Hours";
+            this.btnFiveHr.UseVisualStyleBackColor = true;
+            this.btnFiveHr.Click += new System.EventHandler(this.btnFiveHr_Click);
+            // 
+            // btnEightHrs
+            // 
+            this.btnEightHrs.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEightHrs.Location = new System.Drawing.Point(78, 67);
+            this.btnEightHrs.Name = "btnEightHrs";
+            this.btnEightHrs.Size = new System.Drawing.Size(68, 23);
+            this.btnEightHrs.TabIndex = 8;
+            this.btnEightHrs.Text = "8 Hours";
+            this.btnEightHrs.UseVisualStyleBackColor = true;
+            this.btnEightHrs.Click += new System.EventHandler(this.btnEightHrs_Click);
+            // 
+            // btnTwelveHrs
+            // 
+            this.btnTwelveHrs.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTwelveHrs.Location = new System.Drawing.Point(149, 67);
+            this.btnTwelveHrs.Name = "btnTwelveHrs";
+            this.btnTwelveHrs.Size = new System.Drawing.Size(68, 23);
+            this.btnTwelveHrs.TabIndex = 9;
+            this.btnTwelveHrs.Text = "12Hours";
+            this.btnTwelveHrs.UseVisualStyleBackColor = true;
+            this.btnTwelveHrs.Click += new System.EventHandler(this.btnTwelveHrs_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(12, 194);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 10;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // BtnMainMenu
+            // 
+            this.BtnMainMenu.Location = new System.Drawing.Point(160, 194);
+            this.BtnMainMenu.Name = "BtnMainMenu";
+            this.BtnMainMenu.Size = new System.Drawing.Size(75, 23);
+            this.BtnMainMenu.TabIndex = 11;
+            this.BtnMainMenu.Text = "Main Menu";
+            this.BtnMainMenu.UseVisualStyleBackColor = true;
+            this.BtnMainMenu.Click += new System.EventHandler(this.BtnMainMenu_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(57, 86);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 16);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Distance Traveled";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(42, 48);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(126, 16);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Click a button below";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.groupBox1.Controls.Add(this.btnTwelveHrs);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.btnFiveHr);
+            this.groupBox1.Controls.Add(this.btnEightHrs);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(223, 108);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Input Speed in MPH :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(68, 132);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 16);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "You would travel :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(247, 229);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.BtnMainMenu);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.outPutDistanceTraveled);
+            this.Name = "Form1";
+            this.Text = "Calculate Distance Traveled";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label outPutDistanceTraveled;
+        private System.Windows.Forms.Button btnFiveHr;
+        private System.Windows.Forms.Button btnEightHrs;
+        private System.Windows.Forms.Button btnTwelveHrs;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button BtnMainMenu;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ DistanceTraveled/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DistanceTraveled
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnMainMenu_Click(object sender, EventArgs e)
+        {
+            // if you started from the main menu this will take you back.
+            // otherwise closes the application
+            this.Close();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            // clear all fields
+            textBox1.Text = " ";
+            outPutDistanceTraveled.Text = " ";
+        }
+
+        private void btnFiveHr_Click(object sender, EventArgs e)
+        {
+          int  speed;
+            int time;
+            int distance;
+
+            try
+            {
+                // speed = user input
+                speed = int.Parse(textBox1.Text);
+                // time is set per box
+                time = 5;
+                // distance = speed * time
+                distance = speed * time;
+
+                outPutDistanceTraveled.Text = distance.ToString() + " Miles";
+            }
+            catch
+            {
+                MessageBox.Show("error occured..");
+            }
+
+            
+        }
+
+        private void btnEightHrs_Click(object sender, EventArgs e)
+        {
+            int speed;
+            int time;
+            int distance;
+
+            try
+            {
+                speed = int.Parse(textBox1.Text);
+                time = 8;
+                distance = speed * time;
+
+                outPutDistanceTraveled.Text = distance.ToString() + " Miles";
+            }
+            catch
+            {
+                MessageBox.Show("error occured..");
+            }
+        }
+
+        private void btnTwelveHrs_Click(object sender, EventArgs e)
+        {
+            int speed;
+            int time;
+            int distance;
+
+            try
+            {
+                speed = int.Parse(textBox1.Text);
+                time = 12;
+                distance = speed * time;
+
+                outPutDistanceTraveled.Text = distance.ToString() + " Miles";
+            }
+            catch
+            {
+
+                MessageBox.Show("error..");
+                
+            }
+            
+        }
+    }
+}
