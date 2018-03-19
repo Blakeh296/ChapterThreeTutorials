3409b004fef0eb3c85b7302ff218594813180dda
Blakeh296 <34355831+Blakeh296@users.noreply.github.com>
2018-03-19 15:43:45 -0400

    Add files via upload

---
+++ nameFormatter/Form1.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace nameFormatter
+{
+    partial class nameformatter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1fn = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox2mn = new System.Windows.Forms.TextBox();
+            this.textBox3ln = new System.Windows.Forms.TextBox();
+            this.tbpt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.outPutLabel = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnMainMenu = new System.Windows.Forms.Button();
+            this.btnFormat = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1fn
+            // 
+            this.textBox1fn.Location = new System.Drawing.Point(111, 41);
+            this.textBox1fn.Name = "textBox1fn";
+            this.textBox1fn.Size = new System.Drawing.Size(102, 20);
+            this.textBox1fn.TabIndex = 0;
+            this.textBox1fn.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(11, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(214, 23);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Type following Below :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 16);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Middle Name :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 16);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Last Name :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 120);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(99, 16);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Perferred Title :";
+            // 
+            // textBox2mn
+            // 
+            this.textBox2mn.Location = new System.Drawing.Point(111, 67);
+            this.textBox2mn.Name = "textBox2mn";
+            this.textBox2mn.Size = new System.Drawing.Size(102, 20);
+            this.textBox2mn.TabIndex = 13;
+            this.textBox2mn.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox3ln
+            // 
+            this.textBox3ln.Location = new System.Drawing.Point(111, 93);
+            this.textBox3ln.Name = "textBox3ln";
+            this.textBox3ln.Size = new System.Drawing.Size(102, 20);
+            this.textBox3ln.TabIndex = 14;
+            this.textBox3ln.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tbpt
+            // 
+            this.tbpt.Location = new System.Drawing.Point(111, 119);
+            this.tbpt.Name = "tbpt";
+            this.tbpt.Size = new System.Drawing.Size(102, 20);
+            this.tbpt.TabIndex = 15;
+            this.tbpt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 16);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "First Name :";
+            // 
+            // outPutLabel
+            // 
+            this.outPutLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.outPutLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outPutLabel.Location = new System.Drawing.Point(71, 191);
+            this.outPutLabel.Name = "outPutLabel";
+            this.outPutLabel.Size = new System.Drawing.Size(306, 36);
+            this.outPutLabel.TabIndex = 17;
+            this.outPutLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton6);
+            this.groupBox1.Controls.Add(this.radioButton5);
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(221, 9);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(207, 172);
+            this.groupBox1.TabIndex = 18;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Pick your favorite";
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton6.Location = new System.Drawing.Point(7, 144);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(115, 20);
+            this.radioButton6.TabIndex = 5;
+            this.radioButton6.Text = "Lname, Fname";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton5.Location = new System.Drawing.Point(7, 118);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(166, 20);
+            this.radioButton5.TabIndex = 4;
+            this.radioButton5.Text = "Lname, Fname, Mname";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton4.Location = new System.Drawing.Point(6, 92);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(198, 20);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.Text = "Lname, Fname, Mname, Title";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton3.Location = new System.Drawing.Point(7, 66);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(115, 20);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.Text = "Fname, Lname";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton2.Location = new System.Drawing.Point(7, 40);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(166, 20);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "Fname, Mname, Lname";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton1.Location = new System.Drawing.Point(6, 14);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(198, 20);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Title, Fname, Mname, Lname";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(138, 239);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 19;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnMainMenu
+            // 
+            this.btnMainMenu.Location = new System.Drawing.Point(221, 239);
+            this.btnMainMenu.Name = "btnMainMenu";
+            this.btnMainMenu.Size = new System.Drawing.Size(75, 23);
+            this.btnMainMenu.TabIndex = 20;
+            this.btnMainMenu.Text = "Main Menu";
+            this.btnMainMenu.UseVisualStyleBackColor = true;
+            this.btnMainMenu.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnFormat
+            // 
+            this.btnFormat.Location = new System.Drawing.Point(138, 153);
+            this.btnFormat.Name = "btnFormat";
+            this.btnFormat.Size = new System.Drawing.Size(75, 23);
+            this.btnFormat.TabIndex = 21;
+            this.btnFormat.Text = "Format";
+            this.btnFormat.UseVisualStyleBackColor = true;
+            this.btnFormat.Click += new System.EventHandler(this.btnFormat_Click);
+            // 
+            // nameformatter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(459, 270);
+            this.Controls.Add(this.btnFormat);
+            this.Controls.Add(this.btnMainMenu);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.outPutLabel);
+            this.Controls.Add(this.tbpt);
+            this.Controls.Add(this.textBox3ln);
+            this.Controls.Add(this.textBox2mn);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1fn);
+            this.Name = "nameformatter";
+            this.Text = "Name Formatter";
+            this.Load += new System.EventHandler(this.fNameFormatter_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1fn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox2mn;
+        private System.Windows.Forms.TextBox textBox3ln;
+        private System.Windows.Forms.TextBox tbpt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label outPutLabel;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnMainMenu;
+        private System.Windows.Forms.Button btnFormat;
+    }
+}
+
---
+++ nameFormatter/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace nameFormatter
+{
+    public partial class nameformatter : Form
+    {
+        public nameformatter()
+        {
+            InitializeComponent();
+        }
+
+
+
+        
+
+
+
+        
+        
+
+        private void fNameFormatter_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        
+            
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            // clear all fields
+            outPutLabel.Text = " ";
+            textBox1fn.Text = " ";
+            textBox2mn.Text = " ";
+            textBox3ln.Text = " ";
+            tbpt.Text = " ";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // if you started from the main menu this will take you back.
+            // otherwise closes the application
+            this.Close();
+        }
+
+        
+        private void btnFormat_Click(object sender, EventArgs e)
+        {
+            string name;
+            try
+            {
+                // Names are CONCATENATED in various orders based on which radio button is selected
+                if (radioButton1.Checked)
+                {
+
+                    name = tbpt.Text + " " + textBox1fn.Text + " " + textBox2mn.Text + " " + textBox3ln.Text;
+
+                    outPutLabel.Text = name.ToString();
+                }
+
+                else if (radioButton2.Checked)
+                {
+
+                    name = textBox1fn.Text + " " + textBox2mn.Text + " " + textBox3ln.Text;
+
+                    outPutLabel.Text = name;
+                }
+                else if (radioButton3.Checked)
+                {
+
+                    name = textBox1fn.Text + " " + textBox3ln.Text;
+
+                    outPutLabel.Text = name;
+                }
+
+                else if (radioButton4.Checked)
+                {
+
+                    name = textBox3ln.Text + ", " + textBox1fn.Text + " " + textBox2mn.Text + ", " + tbpt.Text;
+                    outPutLabel.Text = name;
+                }
+
+                else if (radioButton5.Checked)
+                {
+
+                    name = textBox3ln.Text + ", " + textBox1fn.Text + " " + textBox2mn.Text;
+                    outPutLabel.Text = name;
+                }
+
+                else if (radioButton6.Checked)
+                {
+
+                    name = textBox3ln.Text + ", " + textBox1fn.Text;
+                    outPutLabel.Text = name;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Oops..");
+            }
+
+        }
+    
+    }
+
+       
+}
