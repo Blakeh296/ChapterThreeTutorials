9fd229edb766dded1eb10bb346486b71a04fb235
Blakeh296 <34355831+Blakeh296@users.noreply.github.com>
2018-03-19 15:45:02 -0400

    Add files via upload

---
+++ salesTaxandTotal/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace salesTaxandTotal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbPurchaseAmount = new System.Windows.Forms.TextBox();
+            this.salesTaxlLabel = new System.Windows.Forms.Label();
+            this.countyTaxLabel = new System.Windows.Forms.Label();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnMainMenu = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tbPurchaseAmount
+            // 
+            this.tbPurchaseAmount.Location = new System.Drawing.Point(209, 12);
+            this.tbPurchaseAmount.Name = "tbPurchaseAmount";
+            this.tbPurchaseAmount.Size = new System.Drawing.Size(63, 20);
+            this.tbPurchaseAmount.TabIndex = 0;
+            // 
+            // salesTaxlLabel
+            // 
+            this.salesTaxlLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.salesTaxlLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.salesTaxlLabel.Location = new System.Drawing.Point(171, 16);
+            this.salesTaxlLabel.Name = "salesTaxlLabel";
+            this.salesTaxlLabel.Size = new System.Drawing.Size(86, 23);
+            this.salesTaxlLabel.TabIndex = 1;
+            this.salesTaxlLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // countyTaxLabel
+            // 
+            this.countyTaxLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.countyTaxLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.countyTaxLabel.Location = new System.Drawing.Point(171, 49);
+            this.countyTaxLabel.Name = "countyTaxLabel";
+            this.countyTaxLabel.Size = new System.Drawing.Size(86, 23);
+            this.countyTaxLabel.TabIndex = 2;
+            this.countyTaxLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.countyTaxLabel.Click += new System.EventHandler(this.countyTaxLabel_Click);
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.totalLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalLabel.Location = new System.Drawing.Point(171, 82);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(86, 23);
+            this.totalLabel.TabIndex = 3;
+            this.totalLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(182, 117);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 4;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(161, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Purchase Amount :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(6, 21);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(108, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Sales Tax  (4%) :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(6, 54);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(111, 16);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "County Tax (2%) :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(6, 87);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(90, 16);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Total of Sale :";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(101, 117);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 9;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnMainMenu
+            // 
+            this.btnMainMenu.Location = new System.Drawing.Point(197, 195);
+            this.btnMainMenu.Name = "btnMainMenu";
+            this.btnMainMenu.Size = new System.Drawing.Size(75, 23);
+            this.btnMainMenu.TabIndex = 10;
+            this.btnMainMenu.Text = "Main Menu";
+            this.btnMainMenu.UseVisualStyleBackColor = true;
+            this.btnMainMenu.Click += new System.EventHandler(this.btnMainMenu_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.totalLabel);
+            this.groupBox1.Controls.Add(this.salesTaxlLabel);
+            this.groupBox1.Controls.Add(this.btnClear);
+            this.groupBox1.Controls.Add(this.countyTaxLabel);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.btnCalculate);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Location = new System.Drawing.Point(15, 38);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(271, 151);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(298, 230);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnMainMenu);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbPurchaseAmount);
+            this.Name = "Form1";
+            this.Text = "V";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbPurchaseAmount;
+        private System.Windows.Forms.Label salesTaxlLabel;
+        private System.Windows.Forms.Label countyTaxLabel;
+        private System.Windows.Forms.Label totalLabel;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnMainMenu;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ salesTaxandTotal/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace salesTaxandTotal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            double salesTax;
+            double countyTax;
+            double totalOfSale;
+
+            try
+            {
+                // multiply the variables by the various required amounts
+                salesTax = double.Parse(tbPurchaseAmount.Text) * 0.04;
+                countyTax = double.Parse(tbPurchaseAmount.Text) * 0.02;
+
+                // adds everything together
+                totalOfSale = double.Parse(tbPurchaseAmount.Text) + salesTax + countyTax;
+
+                // outputs variables in the form of money "c"
+                salesTaxlLabel.Text = salesTax.ToString("C");
+                countyTaxLabel.Text = countyTax.ToString("C");
+                totalLabel.Text = totalOfSale.ToString("C");
+            }
+            catch
+            {
+                MessageBox.Show("error occured..");
+            }
+           
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            // clear all fields
+            salesTaxlLabel.Text = " ";
+            countyTaxLabel.Text = " ";
+            totalLabel.Text = " ";
+            tbPurchaseAmount.Text = " ";
+        }
+
+        private void btnMainMenu_Click(object sender, EventArgs e)
+        {
+            // if you started from the main menu this will take you back.
+            // otherwise closes the application
+            this.Close();
+        }
+
+        private void countyTaxLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
