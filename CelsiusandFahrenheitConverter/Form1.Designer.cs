44535671dac0cba2b752c6ef7484ba470a54d740
Blakeh296 <34355831+Blakeh296@users.noreply.github.com>
2018-03-19 15:41:49 -0400

    Add files via upload

---
+++ CelsiusandFahrenheitConverter/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace CelsiusandFahrenheitConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFahrtoCel = new System.Windows.Forms.Button();
+            this.btnCeltoFahr = new System.Windows.Forms.Button();
+            this.outPutLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnMainMenu = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnFahrtoCel
+            // 
+            this.btnFahrtoCel.Location = new System.Drawing.Point(108, 82);
+            this.btnFahrtoCel.Name = "btnFahrtoCel";
+            this.btnFahrtoCel.Size = new System.Drawing.Size(96, 23);
+            this.btnFahrtoCel.TabIndex = 0;
+            this.btnFahrtoCel.Text = "to Celsius";
+            this.btnFahrtoCel.UseVisualStyleBackColor = true;
+            this.btnFahrtoCel.Click += new System.EventHandler(this.btnFahrtoCel_Click);
+            // 
+            // btnCeltoFahr
+            // 
+            this.btnCeltoFahr.Location = new System.Drawing.Point(6, 82);
+            this.btnCeltoFahr.Name = "btnCeltoFahr";
+            this.btnCeltoFahr.Size = new System.Drawing.Size(96, 23);
+            this.btnCeltoFahr.TabIndex = 1;
+            this.btnCeltoFahr.Text = "to Fahrenheit";
+            this.btnCeltoFahr.UseVisualStyleBackColor = true;
+            this.btnCeltoFahr.Click += new System.EventHandler(this.btnCeltoFahr_Click);
+            // 
+            // outPutLabel
+            // 
+            this.outPutLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.outPutLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outPutLabel.Location = new System.Drawing.Point(97, 191);
+            this.outPutLabel.Name = "outPutLabel";
+            this.outPutLabel.Size = new System.Drawing.Size(100, 23);
+            this.outPutLabel.TabIndex = 2;
+            this.outPutLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(75, 166);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Converted Temperature :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(118, 21);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(86, 22);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(268, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Convert to Fahrenheit or Celsius";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btnFahrtoCel);
+            this.groupBox1.Controls.Add(this.btnCeltoFahr);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(43, 43);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(217, 111);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Input Your Temperature :";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(85, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(23, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "-->";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(173, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Then Pick Your Conversion!";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(12, 226);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 8;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnMainMenu
+            // 
+            this.btnMainMenu.Location = new System.Drawing.Point(211, 226);
+            this.btnMainMenu.Name = "btnMainMenu";
+            this.btnMainMenu.Size = new System.Drawing.Size(75, 23);
+            this.btnMainMenu.TabIndex = 9;
+            this.btnMainMenu.Text = "Main Menu";
+            this.btnMainMenu.UseVisualStyleBackColor = true;
+            this.btnMainMenu.Click += new System.EventHandler(this.btnMainMenu_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(298, 261);
+            this.Controls.Add(this.btnMainMenu);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.outPutLabel);
+            this.Name = "Form1";
+            this.Text = "Celsius and Fahrenheit Temperature Converter";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFahrtoCel;
+        private System.Windows.Forms.Button btnCeltoFahr;
+        private System.Windows.Forms.Label outPutLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnMainMenu;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ CelsiusandFahrenheitConverter/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CelsiusandFahrenheitConverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCeltoFahr_Click(object sender, EventArgs e)
+        {
+            // use a var for each 
+
+            // will be input temp
+            int cel;
+
+            // will be conversion math
+            int convertToFahr;
+
+            try
+            {
+                // cell = user input text
+                cel = int.Parse(textBox1.Text);
+
+                // take that text value and do some math with converttoFahr
+                convertToFahr = 9 / 5 * cel + 32;
+
+                outPutLabel.Text = convertToFahr.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Unknown error occured..");
+            }
+            
+        }
+
+        private void btnFahrtoCel_Click(object sender, EventArgs e)
+        {
+            // will be input temp
+            int fahr;
+            // var for conversion math
+            int convertToCel;
+
+            try
+            {
+                // set fahr = to the user input
+                fahr = int.Parse(textBox1.Text);
+                // do conversion math with fahr variable with vonverttocell
+                convertToCel = 9 / 5 * (fahr - 32);
+
+                // output conversion to the user
+                outPutLabel.Text = convertToCel.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("error occured..");
+            }
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            // reset all
+            textBox1.Text = " ";
+            outPutLabel.Text = " ";
+        }
+
+        private void btnMainMenu_Click(object sender, EventArgs e)
+        {
+            // if you started from the main menu this will take you back.
+            // otherwise closes the application
+            this.Close();
+        }
+    }
+}
